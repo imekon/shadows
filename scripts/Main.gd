363a2d2b719de5a070d572926ef0c1037102532b
Pete Goodwin <imekon@users.noreply.github.com>
2019-10-16 19:23:42 +0100

    add random crates dropping

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,9 +1,17 @@
 extends Spatial
 
 onready var craft = $Craft
+onready var crateSpawnPoint = $CrateSpawnPoint
 
 const SPEED = 1
 const RANGE = 20
+const CRATE_OFFSET = 2
+
+var crate
+
+func _ready():
+	randomize()
+	crate = load("res://scenes/Crate.tscn")
 
 func _process(delta):
 	var z = craft.translation.z
@@ -11,3 +19,9 @@ func _process(delta):
 	if z > RANGE:
 		z = -RANGE
 	craft.translation.z = z
+
+func on_crate_timeout():
+	var inst = crate.instance()
+	inst.translation = crateSpawnPoint.translation
+	inst.translate(Vector3(randf() * CRATE_OFFSET, 0, randf() * CRATE_OFFSET))
+	add_child(inst)
